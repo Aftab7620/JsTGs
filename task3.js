f02829fa51dc319245f024c47a172adcf6fdcb1c
aftab7620 <mohdaftab7620@gmail.com>
2025-08-08 17:41:28 +0530

    Added Functionality for the div

---
+++ task3.js
@@ -0,0 +1,9 @@
+
+document.getElementById("addButton").addEventListener("click", function () {
+    const container = document.getElementById("addDiv");
+
+    const newBox = document.createElement("div");
+    newBox.classList.add("box");
+
+    container.appendChild(newBox);
+});
