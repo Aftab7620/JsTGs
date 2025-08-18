7030773d6c7f0884e136d71d67e9a586cfb30919
aftab7620 <mohdaftab7620@gmail.com>
2025-08-18 19:44:47 +0530

    js logic

--- LwcFolder/LwcTgs/force-app/main/default/lwc/dataTableTask/dataTableTask.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/dataTableTask/dataTableTask.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
+import acData from '@salesforce/apex/AccountDisplayClass.getAccounts';
 
-export default class DataTableTask extends LightningElement {}
+export default class DataTableTask extends LightningElement {
+    @track data=[];
+    @track columns =[
+        {label: 'Account Name',fieldName: 'Name',type: 'text'},
+        { label: 'Account Phone',fieldName: 'Phone',type:'text'},
+        { label: 'Account Industry',fieldName: 'Industry',type:'text'},
+        { label: 'Description', fieldName: 'Description', type: 'text' }
+    ]
+
+    handleClick(){
+        acData().then(result=>{
+            this.data=result;
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+}
