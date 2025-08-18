12856801f6ab4e98d490b2fdbbd68cf289ab58da
aftab7620 <mohdaftab7620@gmail.com>
2025-08-18 20:10:56 +0530

    html fixed

--- LwcFolder/LwcTgs/force-app/main/default/lwc/dataTableTask/dataTableTask.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/dataTableTask/dataTableTask.js
@@ -4,15 +4,17 @@ import acData from '@salesforce/apex/AccountDisplayClass.getAccounts';
 export default class DataTableTask extends LightningElement {
     @track data=[];
     @track columns =[
+        { label: 'Account Id', fieldName: 'Id', type: 'text' },
         {label: 'Account Name',fieldName: 'Name',type: 'text'},
-        { label: 'Account Phone',fieldName: 'Phone',type:'text'},
-        { label: 'Account Industry',fieldName: 'Industry',type:'text'},
+       { label: 'Account Phone', fieldName: 'Phone', type: 'phone' },
+
         { label: 'Description', fieldName: 'Description', type: 'text' }
     ]
 
     handleClick(){
         acData().then(result=>{
             this.data=result;
+            console.log('Accounts:', result); // Debug
         })
         .catch(error=>{
             console.log(error);
