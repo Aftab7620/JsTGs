ba4379e611e4cadca3b5defdf7d21e95171db09b
aftab7620 <mohdaftab7620@gmail.com>
2025-08-18 18:47:45 +0530

    My local changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 for(let i=1;i<=5;i++){
     console.log(i);
 }
@@ -8,3 +9,21 @@ for(let n=1;n<=5;n++){
     sum=sum+n;
     console.log(sum);
 }
+=======
+// // for(let i=1;i<=5;i++){
+// //     console.log(i);
+// // }
+// // let sum=0;
+// // for(let n=1;n<=5;n++){
+    
+// //     sum=sum+n;
+// //     console.log(sum);
+// // }
+
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+   
+// }
+
+>>>>>>> 7be49ef (My local changes)
