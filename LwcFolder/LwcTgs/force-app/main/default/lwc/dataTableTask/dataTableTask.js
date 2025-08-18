22469e14c39eb1a8f47573dbc8caa165e6fe1953
aftab7620 <mohdaftab7620@gmail.com>
2025-08-18 20:56:52 +0530

    delete feature aded

--- LwcFolder/LwcTgs/force-app/main/default/lwc/dataTableTask/dataTableTask.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/dataTableTask/dataTableTask.js
@@ -1,5 +1,6 @@
 import { LightningElement,track } from 'lwc';
 import acData from '@salesforce/apex/AccountDisplayClass.getAccounts';
+import delData from '@salesforce/apex/AccountDisplayClass.delAccount';
 
 export default class DataTableTask extends LightningElement {
     @track data=[];
@@ -8,7 +9,15 @@ export default class DataTableTask extends LightningElement {
         {label: 'Account Name',fieldName: 'Name',type: 'text'},
        { label: 'Account Phone', fieldName: 'Phone', type: 'phone' },
 
-        { label: 'Description', fieldName: 'Description', type: 'text' }
+        { label: 'Description', fieldName: 'Description', type: 'text' },
+         {
+            type: 'button',
+            typeAttributes: {
+                label: 'Delete',
+                name: 'delete',
+                variant: 'destructive'
+            }
+        }
     ]
 
     handleClick(){
@@ -20,4 +29,19 @@ export default class DataTableTask extends LightningElement {
             console.log(error);
         })
     }
+
+ actionHandle(event) {
+    const row = event.detail.row;
+    delData({ accid: row.Id })
+        .then(() => {
+            this.data = this.data.filter(acc => acc.Id !== row.Id); // UI update
+            console.log('Deleted successfully:', row.Id);
+        })
+        .catch(error => {
+            console.error('Error deleting:', error.body ? error.body.message : error);
+        });
+}
+
+
+
 }
