e56e67eed53f2ecd7f365c0c413b1bd0d835b909
aftab7620 <mohdaftab7620@gmail.com>
2025-08-20 17:38:08 +0530

    custom combo box

--- LwcFolder/LwcTgs/force-app/main/default/lwc/stdComboBoxComp/stdComboBoxComp.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/stdComboBoxComp/stdComboBoxComp.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import conData from '@salesforce/apex/ContactSearchController.searchForContacts';
 
-export default class StdComboBoxComp extends LightningElement {}
+export default class StdComboBoxComp extends LightningElement {
+
+    @track txt = '';                     
+    @track data = [];                   
+   @track valueChoosen='';  
+
+   
+    @wire(conData, {searchText:'$txt'})
+    contacts({ error, data }) {
+        if (data) this.data = data.map(name => ({label:name.Name,value:name.Name }));
+            if (this.data.length === 0) {
+                this.data = [{ label: 'No contacts found', value: '' }];
+            }
+        else if (error) {
+           console.error('Error retrieving contacts:', error);
+        }
+    }
+
+    handleChange(event) {
+        this.txt = event.target.value;
+    }
+
+   
+    handleSelect(event) {
+        this.valueChoosen = event.detail.value;  
+        
+    }
+}
