86a736eaba06d417c04dad34f1e6763a52828ecf
aftab7620 <mohdaftab7620@gmail.com>
2025-08-20 13:04:21 +0530

    added wire call functionality

--- LwcFolder/LwcTgs/force-app/main/default/lwc/wireDataTableTask/wireDataTableTask.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/wireDataTableTask/wireDataTableTask.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track,wire } from 'lwc';
+import wireData from '@salesforce/apex/wireAccountClass.getAccountsWire';
 
-export default class WireDataTableTask extends LightningElement {}
+export default class WireDataTableTask extends LightningElement {
+
+    @track data=[];
+    @track colomn=[
+        { label: 'Account Id', fieldName: 'Id', type: 'text' },
+        {label: 'Account Name',fieldName: 'Name',type: 'text'},
+        {label: 'Account Phone', fieldName: 'Phone', type: 'phone' },
+        { label: 'Description', fieldName: 'Description', type: 'text' },
+    ]
+
+    @wire(wireData)
+    acData({error,data}) {
+        if(data){
+            this.data=data;
+        }
+        else if(error){
+            console.log(error);
+        }
+    }
+}
