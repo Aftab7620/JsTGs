e5586a5aaaedbda2002b7feceb894981ee5ac043
aftab7620 <mohdaftab7620@gmail.com>
2025-08-21 13:00:35 +0530

    js modified

--- LwcFolder/LwcTgs/force-app/main/default/lwc/comboBox/comboBox.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/comboBox/comboBox.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
 
-export default class ComboBox extends LightningElement {}
+export default class ComboBox extends LightningElement {
+
+    get items() {
+        return [
+            {
+                "label": "INDIA",
+                "value": "IN"
+            },
+            {
+                "label": "USA",
+                "value": "US"
+            },
+            {
+                "label": "AUSTRALIA",
+                "value":"AU"
+            },
+            {
+                "label": "NEWZEALAND",
+                "value":"NZ"
+}]
+    }
+}
