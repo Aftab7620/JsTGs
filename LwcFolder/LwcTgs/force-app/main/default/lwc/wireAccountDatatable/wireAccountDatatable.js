03650148fced33b55e28352fd1a91baf58c687c5
aftab7620 <mohdaftab7620@gmail.com>
2025-08-22 12:37:47 +0530

    bug

--- LwcFolder/LwcTgs/force-app/main/default/lwc/wireAccountDatatable/wireAccountDatatable.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/wireAccountDatatable/wireAccountDatatable.js
@@ -1,9 +1,13 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import acData from '@salesforce/apex/accountWireClass.getAccounts';
 
 export default class WireAccountDatatable extends LightningElement {
 
     input;
+
+    @track colomn=[
+        {label}
+    ]
 
     handleName(event){
         this.input=event.target.value;
