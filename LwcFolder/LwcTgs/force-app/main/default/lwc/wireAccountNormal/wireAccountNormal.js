0a416be875ca9e2ef97650314d8e74b5fb22f9eb
aftab7620 <mohdaftab7620@gmail.com>
2025-08-22 12:14:20 +0530

    wire code for Account

--- LwcFolder/LwcTgs/force-app/main/default/lwc/wireAccountNormal/wireAccountNormal.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/wireAccountNormal/wireAccountNormal.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
+import acData from '@salesforce/apex/accountWireClass.getAccounts';
 
-export default class WireAccountNormal extends LightningElement {}
+export default class WireAccountNormal extends LightningElement {
+
+    input;
+
+    @wire(acData)
+    accData;
+
+    handleName(event){
+        this.input=event.target.value;
+    }
+}
