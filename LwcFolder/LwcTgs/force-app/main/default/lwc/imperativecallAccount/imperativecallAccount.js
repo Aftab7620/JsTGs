46443e702ca8dbd07539017d470bbb69b9671e44
aftab7620 <mohdaftab7620@gmail.com>
2025-08-22 18:27:37 +0530

    imperative account

--- LwcFolder/LwcTgs/force-app/main/default/lwc/imperativecallAccount/imperativecallAccount.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/imperativecallAccount/imperativecallAccount.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
+import acData from '@salesforce/apex/imperCallAccount.insertAcc';
 
-export default class ImperativecallAccount extends LightningElement {}
+export default class ImperativecallAccount extends LightningElement {
+    input='';
+
+    handleName(event){
+        this.input=event.target.value;
+    }
+
+    insertAcc(){
+        acData({accName:this.input})
+        .then(result => {
+            alert('inserted');
+        })
+        .catch(error => {
+            alert('error');})
+    }
+}
