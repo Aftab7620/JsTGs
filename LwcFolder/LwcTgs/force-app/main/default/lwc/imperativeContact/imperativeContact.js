28b2b71d809597cf6fdc7e356492046517d563b3
aftab7620 <mohdaftab7620@gmail.com>
2025-08-22 19:02:26 +0530

    imperative call for contact

--- LwcFolder/LwcTgs/force-app/main/default/lwc/imperativeContact/imperativeContact.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/imperativeContact/imperativeContact.js
@@ -1,5 +1,33 @@
 import { LightningElement } from 'lwc';
+import conData from '@salesforce/apex/imperativeContactClass.insertContact';
 
 export default class ImperativeContact extends LightningElement {
-    
+
+    firstName='';
+    lastName='';
+    contact='';
+
+    handleFname(event){
+        this.firstName=event.target.value;
+    }
+
+    handleLname(event){
+        this.lastName=event.target.value;
+    }
+
+    handlePhone(event){
+        this.contact=event.target.value;
+    }
+
+    handleSubmit(){
+        conData({fname:this.firstName,lname:this.lastName,phone:this.contact})
+        .then(result => {
+            alert('inserted');
+        })
+        .catch(error => {
+            alert(error);
+        })
+        
+
+    }
 }
