976e733c221f1defaffcd9e7037391fd423bed36
aftab7620 <mohdaftab7620@gmail.com>
2025-08-22 18:40:37 +0530

    apex class for contact creation

--- LwcFolder/LwcTgs/force-app/main/default/lwc/imperativeContact/imperativeContact.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/imperativeContact/imperativeContact.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class ImperativeContact extends LightningElement {}
+export default class ImperativeContact extends LightningElement {
+    
+}
