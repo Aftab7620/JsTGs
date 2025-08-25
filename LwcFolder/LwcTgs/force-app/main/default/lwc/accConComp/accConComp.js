b3c67f413ffa0be549b7e90841d784d3fd22b4d9
aftab7620 <mohdaftab7620@gmail.com>
2025-08-25 12:12:24 +0530

    acc con comp

--- LwcFolder/LwcTgs/force-app/main/default/lwc/accConComp/accConComp.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/accConComp/accConComp.js
@@ -1,5 +1,21 @@
 import { LightningElement } from 'lwc';
 
 export default class AccConComp extends LightningElement {
+
+    accData='';
+    handleAccount(event){
+        this.accData=event.detail;
+        console.log(this.accData);
+    }
+
+    firstName='';
+    handleFname(event){
+        this.firstName=event.detail;
+    }
+
+    lastName='';
+    handleLname(event){
+        this.lastName=event.detail;
+    }
     
 }
