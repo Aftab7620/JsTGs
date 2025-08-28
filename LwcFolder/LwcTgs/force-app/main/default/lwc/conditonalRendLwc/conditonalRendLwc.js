27a4b107e775d4b1e361190abadb78505aca8209
aftab7620 <mohdaftab7620@gmail.com>
2025-08-28 15:01:59 +0530

    lwc:if demo

--- LwcFolder/LwcTgs/force-app/main/default/lwc/conditonalRendLwc/conditonalRendLwc.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/conditonalRendLwc/conditonalRendLwc.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class ConditonalRendLwc extends LightningElement {}
+export default class ConditonalRendLwc extends LightningElement {
+
+    show=false;
+
+    handleClick(){
+        this.show=!this.show;
+    }
+}
