ed1401b54bf2b69d0ca929542822f9a3236b3da4
aftab7620 <mohdaftab7620@gmail.com>
2025-08-29 17:02:06 +0530

    lwc refrences component

--- LwcFolder/LwcTgs/force-app/main/default/lwc/lwcref/lwcref.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/lwcref/lwcref.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class Lwcref extends LightningElement {}
+export default class Lwcref extends LightningElement {
+
+    alertValue(){
+        for(let key in this.refs){
+            let compChildRef=this.refs[key];
+            let property0ne=compChildRef.propOne;
+            let propertyTwo=compChildRef.propTwo;
+            
+            console.log(property0ne);
+            console.log(propertyTwo);
+            compChildRef.alertValue();
+        }
+    }
+}
