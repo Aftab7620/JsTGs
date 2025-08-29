bdcd0570fb24172206c6e34da942d742cb1e9407
aftab7620 <mohdaftab7620@gmail.com>
2025-08-29 15:36:10 +0530

    ref data

--- LwcFolder/LwcTgs/force-app/main/default/lwc/refrenceDemo/refrenceDemo.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/refrenceDemo/refrenceDemo.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class RefrenceDemo extends LightningElement {}
+export default class RefrenceDemo extends LightningElement {
+
+    alertValue(){
+        let compChildRef=this.template.querySelector('c-ref-child');
+        let property0ne=compChildRef.propOne;
+        let propertyTwo=compChildRef.propTwo;
+
+        console.log(property0ne);
+        console.log(propertyTwo);
+        compChildRef.alertValue();
+    }
+}
