f78b94d70107b6fd54c1d88cd7409313f80fce12
aftab7620 <mohdaftab7620@gmail.com>
2025-09-01 12:37:33 +0530

    js added

--- LwcFolder/LwcTgs/force-app/main/default/lwc/lifeCycleHook/lifeCycleHook.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/lifeCycleHook/lifeCycleHook.js
@@ -1,3 +1,27 @@
 import { LightningElement } from 'lwc';
 
-export default class LifeCycleHook extends LightningElement {}
+export default class LifeCycleHook extends LightningElement {
+
+    dummy='Test';
+
+    constructor(){
+        super();
+        console.log('Constructor Called');
+    }
+
+    connectedCallback(){
+        console.log('Connected Callback Called');
+    }
+
+    renderedCallback(){
+        console.log('Rendered Callback Called');
+    }
+
+    disconnectedCallback(){
+        console.log('Disconnected Callback Called');
+    }
+
+    handleChange(event){
+        this.dummy=event.target.value;
+    }
+}
