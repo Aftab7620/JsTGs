a512a8812a7e5706da4339c3dd3db4c320f545f3
aftab7620 <mohdaftab7620@gmail.com>
2025-09-03 19:04:54 +0530

    html

--- LwcFolder/LwcTgs/force-app/main/default/lwc/ldsRecordEditFormRev/ldsRecordEditFormRev.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/ldsRecordEditFormRev/ldsRecordEditFormRev.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
 
-export default class LdsRecordEditFormRev extends LightningElement {}
+
+export default class LdsRecordEditFormRev extends LightningElement {
+    nameField=NAME_FIELD;
+}
