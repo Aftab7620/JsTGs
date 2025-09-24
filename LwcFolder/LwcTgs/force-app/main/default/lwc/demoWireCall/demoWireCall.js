1a8d0f1328e0bf14880886b5d9f7d079f66667f0
aftab7620 <mohdaftab7620@gmail.com>
2025-09-24 12:55:54 +0530

    js code and html

--- LwcFolder/LwcTgs/force-app/main/default/lwc/demoWireCall/demoWireCall.js
+++ LwcFolder/LwcTgs/force-app/main/default/lwc/demoWireCall/demoWireCall.js
@@ -1,3 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import conData from '@salesforce/apex/conClass.getAccounts';
 
-export default class DemoWireCall extends LightningElement {}
+export default class DemoWireCall extends LightningElement {
+
+    @track column=[
+        {label:'Name',fieldName:'Name'},
+        {label:'Type',fieldName:'Type'},
+        {label:'Phone',fieldName:'Phone'}]
+
+    @track ac=[];
+
+    @wire(conData)
+    wiredData({data,error}){
+        if(data){
+            this.ac=data;
+
+    }
+
+
+}
+}
